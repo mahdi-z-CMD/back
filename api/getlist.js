3e552071b76d60963ba5ed44a1cd24f9a0234f95
M Z <mahdi.zxzxz@gmail.com>
2024-07-22 19:12:23 +0330

    update api urls

--- api/getlist.js
+++ api/getlist.js
@@ -1,6 +1,5 @@
 const { MongoClient } = require('mongodb');
 
-// Replace with your MongoDB URI
 const uri = 'mongodb+srv://hassan:KRgWqofng5lMtGgw@vpn-customers.g7s1zv1.mongodb.net/?retryWrites=true&w=majority&appName=vpn-customers';
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -8,7 +7,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 exports.handler = async (event) => {
   try {
     await client.connect();
-    const database = client.db('customers'); // Updated database name
+    const database = client.db('customers');
     const collection = database.collection('vpn');
 
     const users = await collection.find({}).toArray();
