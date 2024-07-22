f10e9fb71b84855b0aed8afeb22610936cf94080
M Z <mahdi.zxzxz@gmail.com>
2024-07-22 19:44:20 +0330

    update netlify

--- api/getlist.js
+++ api/getlist.js
@@ -1,28 +1,37 @@
-// /api/getUsers.js
+// api/getlist.js
 
 const { MongoClient } = require('mongodb');
 
 const uri = "mongodb+srv://hassan:KRgWqofng5lMtGgw@vpn-customers.g7s1zv1.mongodb.net/?retryWrites=true&w=majority&appName=vpn-customers";
-const client = new MongoClient(uri);
 
 exports.handler = async function(event, context) {
   try {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
-    const database = client.db('customers');
-    const collection = database.collection('vpn');
+    const collection = client.db('customers').collection('vpn');
     
     const users = await collection.find({}).toArray();
     
+    await client.close();
+    
     return {
       statusCode: 200,
-      body: JSON.stringify(users),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*", // Allow requests from any origin
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS", // Allow specific methods
+        "Access-Control-Allow-Headers": "Content-Type" // Allow specific headers
+      },
+      body: JSON.stringify(users)
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to fetch users' }),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({ error: 'Failed to fetch data' })
     };
-  } finally {
-    await client.close();
   }
 };
