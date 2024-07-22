9d7b4f83c4cd54c5c32a2fbf4650cc5866e138b0
M Z <mahdi.zxzxz@gmail.com>
2024-07-22 19:32:02 +0330

    update netlify

--- api/adduser.js
+++ api/adduser.js
@@ -1,45 +1,29 @@
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb+srv://hassan:KRgWqofng5lMtGgw@vpn-customers.g7s1zv1.mongodb.net/?retryWrites=true&w=majority&appName=vpn-customers';
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = "mongodb+srv://hassan:KRgWqofng5lMtGgw@vpn-customers.g7s1zv1.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
 exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
     return {
       statusCode: 405,
-      body: JSON.stringify({ error: 'Method Not Allowed' }),
+      body: JSON.stringify({ error: 'Method not allowed' }),
     };
   }
 
-  const { email, date, telegramId } = JSON.parse(event.body);
-
-  if (!email || !date) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: 'Email and Date are required' }),
-    };
-  }
+  const { email, data, telegramId } = JSON.parse(event.body);
 
   try {
     await client.connect();
     const database = client.db('customers');
     const collection = database.collection('vpn');
 
-    const newUser = {
-      email,
-      date,
-      telegramId: telegramId || null,
-    };
-
-    const result = await collection.insertOne(newUser);
+    const newUser = { email, data, telegramId };
+    await collection.insertOne(newUser);
 
     return {
-      statusCode: 201,
-      body: JSON.stringify(result),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      statusCode: 200,
+      body: JSON.stringify({ message: 'User added successfully' }),
     };
   } catch (error) {
     return {
