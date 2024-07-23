9ce5f03f4eaf1b77baa6861c0bf5972f36041dd3
M Z <mahdi.zxzxz@gmail.com>
2024-07-23 21:09:53 +0330

    edit user and delete added 10

--- api/deleteuser.js
+++ api/deleteuser.js
@@ -5,8 +5,8 @@ const { MongoClient, ObjectId } = require('mongodb');
 exports.handler = async (event) => {
   if (event.httpMethod === 'DELETE') {
     try {
+      // Parse and validate the request body
       const { id } = JSON.parse(event.body);
-
       if (!ObjectId.isValid(id)) {
         return {
           statusCode: 400,
@@ -14,14 +14,16 @@ exports.handler = async (event) => {
         };
       }
 
-      const client = await MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+      // Connect to MongoDB using the URI directly
+      const client = await MongoClient.connect('mongodb+srv://hassan:KRgWqofng5lMtGgw@vpn-customers.g7s1zv1.mongodb.net/?retryWrites=true&w=majority&appName=vpn-customers', { useNewUrlParser: true, useUnifiedTopology: true });
       const db = client.db('customers');
       const collection = db.collection('vpn');
 
+      // Delete the user
       const result = await collection.deleteOne({ _id: new ObjectId(id) });
-
       client.close();
 
+      // Return appropriate response
       if (result.deletedCount > 0) {
         return {
           statusCode: 200,
@@ -34,6 +36,8 @@ exports.handler = async (event) => {
         };
       }
     } catch (error) {
+      // Log and return error message
+      console.error('Error:', error.message);
       return {
         statusCode: 500,
         body: JSON.stringify({ success: false, error: error.message }),
