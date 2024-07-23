ea30d391ac51adbb8eedb45abfe5aeaee57ee192
M Z <mahdi.zxzxz@gmail.com>
2024-07-23 21:13:03 +0330

    edit user and delete added 11

--- api/deleteuser.js
+++ api/deleteuser.js
@@ -5,42 +5,57 @@ const { MongoClient, ObjectId } = require('mongodb');
 exports.handler = async (event) => {
   if (event.httpMethod === 'DELETE') {
     try {
-      // Parse and validate the request body
       const { id } = JSON.parse(event.body);
       if (!ObjectId.isValid(id)) {
         return {
           statusCode: 400,
           body: JSON.stringify({ success: false, message: 'Invalid ID format' }),
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS, POST, PUT, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          },
         };
       }
 
-      // Connect to MongoDB using the URI directly
       const client = await MongoClient.connect('mongodb+srv://hassan:KRgWqofng5lMtGgw@vpn-customers.g7s1zv1.mongodb.net/?retryWrites=true&w=majority&appName=vpn-customers', { useNewUrlParser: true, useUnifiedTopology: true });
       const db = client.db('customers');
       const collection = db.collection('vpn');
 
-      // Delete the user
       const result = await collection.deleteOne({ _id: new ObjectId(id) });
       client.close();
 
-      // Return appropriate response
       if (result.deletedCount > 0) {
         return {
           statusCode: 200,
           body: JSON.stringify({ success: true }),
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS, POST, PUT, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          },
         };
       } else {
         return {
           statusCode: 404,
           body: JSON.stringify({ success: false, message: 'User not found' }),
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS, POST, PUT, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          },
         };
       }
     } catch (error) {
-      // Log and return error message
       console.error('Error:', error.message);
       return {
         statusCode: 500,
         body: JSON.stringify({ success: false, error: error.message }),
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'OPTIONS, POST, PUT, DELETE',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        },
       };
     }
   }
@@ -48,5 +63,10 @@ exports.handler = async (event) => {
   return {
     statusCode: 405,
     body: JSON.stringify({ success: false, message: 'Method not allowed' }),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS, POST, PUT, DELETE',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
   };
 };
