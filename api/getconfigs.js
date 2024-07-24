a3b71f6cdbd5d7376c91e3c8ac3f5ed22da5b6c9
M Z <mahdi.zxzxz@gmail.com>
2024-07-24 15:56:42 +0330

    add configs section

---
+++ api/getconfigs.js
@@ -0,0 +1,37 @@
+// api/getlist.js
+
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb+srv://hassan:KRgWqofng5lMtGgw@vpn-customers.g7s1zv1.mongodb.net/?retryWrites=true&w=majority&appName=vpn-customers";
+
+exports.handler = async function(event, context) {
+  try {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    const collection = client.db('customers').collection('configs');
+    
+    const users = await collection.find({}).toArray();
+    
+    await client.close();
+    
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*", // Allow requests from any origin
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS", // Allow specific methods
+        "Access-Control-Allow-Headers": "Content-Type" // Allow specific headers
+      },
+      body: JSON.stringify(users)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({ error: 'Failed to fetch data' })
+    };
+  }
+};
